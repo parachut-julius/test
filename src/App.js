11774de1c5b5d3de4c2f2ab9d73d395fb5b11709
Julius <jmichaelcodes@gmail.com>
2019-10-01 10:21:48 -0400

    [test-3] aSDF

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="asdfasdfasfasdfasfasffasdf">
+      <header className="aaasss">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
